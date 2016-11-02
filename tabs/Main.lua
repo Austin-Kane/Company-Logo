6efe62dbf636a3db9870e3307ea32a76b1072a79
Austin-Kane at codea-scm <Austin-Kane@codea-scm.aws.mapote.com>
2016-11-02 15:01:26 +0000

    init

---
+++ tabs/Main.lua
@@ -0,0 +1,27 @@
+-- Company Logo
+
+-- Created by: Austin Kane
+-- Created on Oct- 21, 2016
+-- Created for ICS2O
+-- This program shows the company logo
+
+-- Use this function to perform your initial setup
+function setup()
+    supportedOrientations(LANDSCAPE_ANY)
+    displayMode(FULLSCREEN)
+    noFill()
+    noSmooth()
+    noStroke()
+    pushStyle()
+    
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- Do your drawing here
+    sprite("Project:companyLogoImage", WIDTH/2,HEIGHT/2,WIDTH,HEIGHT)    
+end
+
